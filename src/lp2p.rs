954a37fc1a8dd7944849ca50eeeafd139a27556e
gguoss <guanghuaguo.oss@gmail.com>
2019-10-29 14:35:20 +0800

    Add lp2p module on runtime event

---
+++ src/lp2p.rs
@@ -0,0 +1,101 @@
+// Copyright 2019 杭州链网科技
+
+use futures::prelude::*;
+use libp2p::{
+    floodsub::{self, Floodsub, FloodsubEvent},
+    identity,
+    mdns::{Mdns, MdnsEvent},
+    swarm::NetworkBehaviourEventProcess,
+    tokio_io::{AsyncRead, AsyncWrite},
+    NetworkBehaviour, PeerId, Swarm,
+};
+use tokio::runtime::TaskExecutor;
+
+// We create a custom network behaviour that combines floodsub and mDNS.
+// In the future, we want to improve libp2p to make this easier to do.
+#[derive(NetworkBehaviour)]
+struct MyBehaviour<TSubstream: AsyncRead + AsyncWrite> {
+    floodsub: Floodsub<TSubstream>,
+    mdns: Mdns<TSubstream>,
+}
+
+impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<MdnsEvent>
+    for MyBehaviour<TSubstream>
+{
+    fn inject_event(&mut self, event: MdnsEvent) {
+        match event {
+            MdnsEvent::Discovered(list) => {
+                for (peer, _) in list {
+                    self.floodsub.add_node_to_partial_view(peer);
+                }
+            }
+            MdnsEvent::Expired(list) => {
+                for (peer, _) in list {
+                    if !self.mdns.has_node(&peer) {
+                        self.floodsub.remove_node_from_partial_view(&peer);
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<FloodsubEvent>
+    for MyBehaviour<TSubstream>
+{
+    // Called when `floodsub` produces an event.
+    fn inject_event(&mut self, message: FloodsubEvent) {
+        if let FloodsubEvent::Message(message) = message {
+            println!(
+                "Received: '{:?}' from {:?}",
+                String::from_utf8_lossy(&message.data),
+                message.source
+            );
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct NetworkState {
+    listenning: bool,
+}
+
+pub fn initialize(task_executor: TaskExecutor, mut network_state: NetworkState) {
+    // Create a random PeerId
+    let local_key = identity::Keypair::generate_secp256k1();
+    let local_peer_id = PeerId::from(local_key.public());
+    // Set up a an encrypted DNS-enabled TCP Transport over the Mplex and Yamux protocols
+    let transport = libp2p::build_development_transport(local_key);
+
+    // Create a Floodsub topic
+    let floodsub_topic = floodsub::TopicBuilder::new("/fil/messages").build();
+
+    // Create a Swarm to manage peers and events
+    let mut swarm = {
+        let mut behaviour = MyBehaviour {
+            floodsub: Floodsub::new(local_peer_id.clone()),
+            mdns: Mdns::new().expect("Failed to create mDNS service"),
+        };
+
+        behaviour.floodsub.subscribe(floodsub_topic.clone());
+        Swarm::new(transport, behaviour, local_peer_id)
+    };
+
+    // Listen on all interfaces and whatever port the OS assigns
+    Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
+    task_executor.spawn(futures::future::poll_fn(move || -> Result<_, ()> {
+        loop {
+            match swarm.poll().expect("Error while polling swarm") {
+                Async::Ready(Some(_)) => {}
+                Async::Ready(None) | Async::NotReady => {
+                    if !network_state.listenning {
+                        if let Some(a) = Swarm::listeners(&swarm).next() {
+                            println!("Listening on {:?}", a);
+                        }
+                        network_state.listenning = true;
+                    }
+                }
+            }
+        }
+    }));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,14 @@
-// Copyright 2019 PolkaX Team.
+// Copyright 2019 杭州链网科技 Team.
 
-use futures::prelude::*;
-use libp2p::{
-    PeerId,
-    Swarm,
-    NetworkBehaviour,
-    identity,
-    tokio_io::{AsyncRead, AsyncWrite},
-    floodsub::{self, Floodsub, FloodsubEvent},
-    mdns::{Mdns, MdnsEvent},
-    swarm::NetworkBehaviourEventProcess
-};
-
-// We create a custom network behaviour that combines floodsub and mDNS.
-// In the future, we want to improve libp2p to make this easier to do.
-#[derive(NetworkBehaviour)]
-struct MyBehaviour<TSubstream: AsyncRead + AsyncWrite> {
-    floodsub: Floodsub<TSubstream>,
-    mdns: Mdns<TSubstream>,
-}
-
-impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<MdnsEvent> for MyBehaviour<TSubstream> {
-        fn inject_event(&mut self, event: MdnsEvent) {
-            match event {
-                MdnsEvent::Discovered(list) => {
-                    for (peer, _) in list {
-                        self.floodsub.add_node_to_partial_view(peer);
-                    }
-                },
-                MdnsEvent::Expired(list) => {
-                    for (peer, _) in list {
-                        if !self.mdns.has_node(&peer) {
-                            self.floodsub.remove_node_from_partial_view(&peer);
-                        }
-                    }
-                }
-            }
-        }
-}
-
-    impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<FloodsubEvent> for MyBehaviour<TSubstream> {
-        // Called when `floodsub` produces an event.
-        fn inject_event(&mut self, message: FloodsubEvent) {
-            if let FloodsubEvent::Message(message) = message {
-                println!("Received: '{:?}' from {:?}", String::from_utf8_lossy(&message.data), message.source);
-            }
-        }
-    }
+mod lp2p;
+use tokio::runtime::Runtime;
 
 fn main() {
-    // Create a random PeerId
-    let local_key = identity::Keypair::generate_ed25519();
-    let local_peer_id = PeerId::from(local_key.public());
-    // Set up a an encrypted DNS-enabled TCP Transport over the Mplex and Yamux protocols
-    let transport = libp2p::build_development_transport(local_key);
-
-    // Create a Floodsub topic
-    let floodsub_topic = floodsub::TopicBuilder::new("/fil/messages").build();
-
-    // Create a Swarm to manage peers and events
-    let mut swarm = {
-        let mut behaviour = MyBehaviour {
-            floodsub: Floodsub::new(local_peer_id.clone()),
-            mdns: Mdns::new().expect("Failed to create mDNS service"),
-        };
-
-        behaviour.floodsub.subscribe(floodsub_topic.clone());
-        Swarm::new(transport, behaviour, local_peer_id)
-    };
-
-    // Reach out to another node if specified
-    if let Some(to_dial) = std::env::args().nth(1) {
-        let dialing = to_dial.clone();
-        match to_dial.parse() {
-            Ok(to_dial) => {
-                match libp2p::Swarm::dial_addr(&mut swarm, to_dial) {
-                    Ok(_) => println!("Dialed {:?}", dialing),
-                    Err(e) => println!("Dial {:?} failed: {:?}", dialing, e)
-                }
-            },
-            Err(err) => println!("Failed to parse address to dial: {:?}", err),
-        }
-    }
-
-    // Listen on all interfaces and whatever port the OS assigns
-    Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
-
-    // Kick it off
-    let mut listening = false;
-    tokio::run(futures::future::poll_fn(move || -> Result<_, ()> {
-        loop {
-            match swarm.poll().expect("Error while polling swarm") {
-                Async::Ready(Some(_)) => {
-
-                },
-                Async::Ready(None) | Async::NotReady => {
-                    if !listening {
-                        if let Some(a) = Swarm::listeners(&swarm).next() {
-                            println!("Listening on {:?}", a);
-                            listening = true;
-                        }
-                    }
-                    break
-                }
-            }
-        }
-        Ok(Async::NotReady)
-    }));
+    let (exit_send, exit) = exit_future::signal();
+    let mut runtime = Runtime::new().expect("failed to start runtime on current thread");
+    let task_executor = runtime.executor();
+    let network_state = lp2p::NetworkState::default();
+    lp2p::initialize(task_executor, network_state);
+    let _ = runtime.block_on(exit);
+    exit_send.fire();
 }
