3833ea02ebdcc714db687b2a3a3c673ab4755eb3
zhang-dong-dong <2209940496@qq.com>
2019-12-18 14:25:07 +0800

    Add keystore

--- wallet/src/crypto.rs
+++ wallet/src/crypto.rs
@@ -1,5 +1,6 @@
 use blake2_rfc::blake2b::blake2b;
 use data_encoding::Specification;
+use std::convert::TryFrom;
 
 // PayloadHashLength defines the hash length taken over addresses using the Actor and SECP256K1 protocols.
 pub const PAYLOAD_HASH_LENGTH: usize = 20;
@@ -29,6 +30,57 @@ pub fn base32_encode(input: &[u8]) -> String {
     let encoder = spec.encoding().unwrap();
 
     encoder.encode(&input)
+}
+
+
+/// An identifier for a type of cryptographic key.
+///
+/// To avoid clashes with other modules when distributing your module publically, register your
+/// `KeyTypeId` on the list here by making a PR.
+///
+/// Values whose first character is `_` are reserved for private use and won't conflict with any
+/// public modules.
+#[derive(
+Copy, Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash
+)]
+pub struct KeyTypeId(pub [u8; 4]);
+
+impl From<u32> for KeyTypeId {
+    fn from(x: u32) -> Self {
+        Self(x.to_le_bytes())
+    }
+}
+
+impl From<KeyTypeId> for u32 {
+    fn from(x: KeyTypeId) -> Self {
+        u32::from_le_bytes(x.0)
+    }
+}
+
+impl<'a> TryFrom<&'a str> for KeyTypeId {
+    type Error = ();
+    fn try_from(x: &'a str) -> Result<Self, ()> {
+        let b = x.as_bytes();
+        if b.len() != 4 {
+            return Err(());
+        }
+        let mut res = KeyTypeId::default();
+        res.0.copy_from_slice(&b[0..4]);
+        Ok(res)
+    }
+}
+
+/// Known key types; this also functions as a global registry of key types for projects wishing to
+/// avoid collisions with each other.
+///
+/// It's not universal in the sense that *all* key types need to be mentioned here, it's just a
+/// handy place to put common key types.
+pub mod key_types {
+    use super::KeyTypeId;
+    /// Key type for Babe module, build-in.
+    pub const BLS: KeyTypeId = KeyTypeId(*b"bls0");
+    /// Key type for Grandpa module, build-in.
+    pub const SECP256K1: KeyTypeId = KeyTypeId(*b"secp");
 }
 
 #[test]
--- wallet/src/lib.rs
+++ wallet/src/lib.rs
@@ -1,8 +1,62 @@
-pub mod address;
-pub mod crypto;
+use std::{collections::HashMap, path::PathBuf, fs::{self, File}, io::{self, Write}, sync::Arc};
+use crypto::KeyTypeId;
+use parking_lot::RwLock;
 
-// keystore
+mod address;
+mod crypto;
+mod keystore;
 //
 // Generate
 // Sign
 // Remove
+//walletNew,
+//walletList,
+//walletBalance,
+
+/// wallet pointer
+pub type WalletPtr = Arc<RwLock<Wallet>>;
+
+/// Keystore error.
+#[derive(Debug, derive_more::Display, derive_more::From)]
+pub enum Error {
+    /// IO error.
+    Io(io::Error),
+    /// JSON error.
+    Json(serde_json::Error),
+    /// Invalid password.
+    #[display(fmt="Invalid password")]
+    InvalidPassword,
+    /// Invalid BIP39 phrase
+    #[display(fmt="Invalid recovery phrase (BIP39) data")]
+    InvalidPhrase,
+    /// Invalid seed
+    #[display(fmt="Invalid seed")]
+    InvalidSeed,
+    /// Keystore unavailable
+    #[display(fmt="Keystore unavailable")]
+    Unavailable,
+}
+/// wallet Result
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::Io(ref err) => Some(err),
+            Error::Json(ref err) => Some(err),
+            _ => None,
+        }
+    }
+}
+
+pub struct Wallet {
+    keys: HashMap<(KeyTypeId, Vec<u8>), Vec<u8>>,
+    path: PathBuf
+    //password: Option<Protected<String>>,
+}
+
+impl Wallet {
+    pub fn new_address(key_type: KeyTypeId) -> Result<String> {
+        Ok(String::new())
+    }
+}
