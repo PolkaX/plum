45f1147192b8b42a72d8cc0a66ce533d1964be36
koushiro <koushiro.cqx@gmail.com>
2020-03-04 13:25:46 +0800

    Add doc for bigint
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- chain/bigint/src/biguint/json.rs
+++ chain/bigint/src/biguint/json.rs
@@ -5,6 +5,7 @@ use std::str::FromStr;
 use num_bigint::BigUint;
 use serde::{de, ser, Deserialize, Serialize};
 
+/// JSON serialization
 pub fn serialize<S>(uint: &BigUint, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: ser::Serializer,
@@ -12,6 +13,7 @@ where
     uint.to_string().serialize(serializer)
 }
 
+/// JSON deserialization
 pub fn deserialize<'de, D>(deserializer: D) -> Result<BigUint, D::Error>
 where
     D: de::Deserializer<'de>,
