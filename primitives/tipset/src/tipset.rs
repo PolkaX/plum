87bb0914ce7a1e3c355004cfda055e6d05667bfa
koushiro <koushiro.cqx@gmail.com>
2020-03-19 15:20:45 +0800

    Impl some methods of Tipset
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- primitives/tipset/src/tipset.rs
+++ primitives/tipset/src/tipset.rs
@@ -2,22 +2,85 @@
 
 use cid::Cid;
 
+use plum_bigint::BigInt;
 use plum_block::BlockHeader;
+use plum_ticket::Ticket;
 
+use crate::errors::TipsetError;
 use crate::key::TipsetKey;
 
 ///
-#[derive(Eq, PartialEq, Clone, Debug, Hash)]
+#[derive(Clone, Debug)]
 pub struct Tipset {
     key: TipsetKey,
     blocks: Vec<BlockHeader>,
     height: u64,
 }
 
+impl PartialEq for Tipset {
+    fn eq(&self, other: &Self) -> bool {
+        self.blocks.as_slice().eq(other.blocks())
+    }
+}
+
+impl Eq for Tipset {}
+
+impl std::hash::Hash for Tipset {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.blocks.hash(state)
+    }
+}
+
 impl Tipset {
-    ///
-    pub fn new(key: TipsetKey, blocks: Vec<BlockHeader>) -> Self {
-        todo!()
+    /// Create a new Tipset with the given block headers.
+    pub fn new(mut blocks: Vec<BlockHeader>) -> Result<Self, TipsetError> {
+        if blocks.is_empty() {
+            return Err(TipsetError::EmptyBlocks);
+        }
+
+        blocks.sort_by(|i, j| {
+            let ti = i.last_ticket();
+            let tj = j.last_ticket();
+            if ti == tj {
+                // block i and block j have the same ticket, then compare the cid of the two blocks.
+                i.cid().cmp(&j.cid())
+            } else {
+                ti.cmp(tj)
+            }
+        });
+
+        let mut cids = vec![];
+        let height = blocks[0].height;
+        for block in &blocks {
+            if block.height != height {
+                return Err(TipsetError::MismatchingHeight {
+                    expected: height,
+                    found: block.height,
+                });
+            }
+
+            for (i, cid) in block.parents.iter().enumerate() {
+                if cid != &blocks[0].parents[i] {
+                    return Err(TipsetError::MismatchingParent {
+                        expected: blocks[0].parents[i].clone(),
+                        found: cid.clone(),
+                    });
+                }
+            }
+
+            cids.push(block.cid());
+        }
+
+        Ok(Self {
+            key: TipsetKey::new(cids),
+            blocks,
+            height,
+        })
+    }
+
+    /// Determine whether the tipset contains a block header with a Cid value of `cid`.
+    pub fn contains(&self, cid: &Cid) -> bool {
+        self.key.cids().contains(cid)
     }
 
     /// Return the key.
@@ -30,14 +93,54 @@ impl Tipset {
         self.key.cids()
     }
 
-    ///
+    /// Return the block headers.
     pub fn blocks(&self) -> &[BlockHeader] {
         &self.blocks
     }
 
-    ///
+    /// Return the height of the first block.
     pub fn height(&self) -> u64 {
         self.height
+    }
+
+    /// Return the tipset key that represents the parents of the first block.
+    pub fn parents(&self) -> TipsetKey {
+        TipsetKey::new((&self.blocks[0].parents).to_vec())
+    }
+
+    /// Return the parent state of the first block.
+    pub fn parent_state(&self) -> &Cid {
+        &self.blocks[0].parent_state_root
+    }
+
+    /// Return the parent weight of the first block.
+    pub fn parent_weight(&self) -> &BigInt {
+        &self.blocks[0].parent_weight
+    }
+
+    /// Return the block, which has the minimum ticket.
+    pub fn min_ticket_block(&self) -> &BlockHeader {
+        let mut min_block = &self.blocks[0];
+        for block in &self.blocks {
+            if block.last_ticket() < min_block.last_ticket() {
+                min_block = block;
+            }
+        }
+        min_block
+    }
+
+    /// Return the minimum ticket in the blocks.
+    pub fn min_ticket(&self) -> &Ticket {
+        &self.min_ticket_block().ticket
+    }
+
+    /// Return the minimum timestamp in the blocks.
+    pub fn min_timestamp(&self) -> u64 {
+        self.blocks
+            .iter()
+            .map(|block| block.timestamp)
+            .min()
+            .expect("Tipset is not empty")
     }
 }
 
