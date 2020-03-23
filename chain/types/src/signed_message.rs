02e54e0876cc2f3f7a5270dfd86cc0400b4262f3
koushiro <koushiro.cqx@gmail.com>
2020-03-23 13:24:40 +0800

    Remove chain-types and sigs; Rename chain => plum_chain, actors => plum_actor
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- chain/types/src/signed_message.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2019-2020 PolkaX Authors. Licensed under GPL-3.0.
-
-use crate::{message::Message, signature::Signature};
-
-#[derive(Clone, Debug)]
-pub struct SignedMessage {
-    pub message: Message,
-    pub signature: Signature,
-}
