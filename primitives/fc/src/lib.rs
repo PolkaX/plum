b756a225490161cc577b27fb7326c9fbbccaf7cd
Aten <jincxmain@gmail.com>
2020-06-03 18:07:36 +0800

    remove filecoin-proofs (#84)
    1. remove filecoin-proofs crate
    2. remove piece convert into miner(not in this repo)

--- primitives/fc/src/lib.rs
+++ primitives/fc/src/lib.rs
@@ -12,26 +12,7 @@ pub use self::comm::{
     replica_commitment_v1_to_cid, CommCidErr, FILECOIN_CODEC_TYPE,
 };
 
-use filecoin_proofs::types::{PieceInfo as FcPieceInfo, UnpaddedBytesAmount};
+// use filecoin_proofs::types::{PieceInfo as FcPieceInfo, UnpaddedBytesAmount};
 
-use plum_piece::PieceInfo;
+// use plum_piece::PieceInfo;
 
-/// Convert piece info to filecoin proof piece info.
-pub fn convert_pieceinfo(pieceinfo: PieceInfo) -> Result<FcPieceInfo, CommCidErr> {
-    let unpadded = pieceinfo.size.unpadded();
-    let commitment = cid_to_piece_commitment_v1(&pieceinfo.piece_cid)?;
-    Ok(FcPieceInfo {
-        commitment,
-        size: UnpaddedBytesAmount(u64::from(unpadded)),
-    })
-}
-
-/// convert piece info list to filecoin proof pieceinfo list
-pub fn convert_pieceinfos(pieceinfos: Vec<PieceInfo>) -> Result<Vec<FcPieceInfo>, CommCidErr> {
-    let mut v = Vec::with_capacity(pieceinfos.len());
-    for info in pieceinfos {
-        let p = convert_pieceinfo(info)?;
-        v.push(p);
-    }
-    Ok(v)
-}
