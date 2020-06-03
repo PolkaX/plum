756663b10b98e55ec2c4a0b24c79a6855d3c0ae6
Aten <jincxmain@gmail.com>
2020-06-03 20:45:06 +0800

    move `to_prove_id` to plum_fc crate

--- primitives/fc/src/lib.rs
+++ primitives/fc/src/lib.rs
@@ -12,7 +12,15 @@ pub use self::comm::{
     replica_commitment_v1_to_cid, CommCidErr, FILECOIN_CODEC_TYPE,
 };
 
-// use filecoin_proofs::types::{PieceInfo as FcPieceInfo, UnpaddedBytesAmount};
+use plum_address::{Address, AddressError};
+use plum_types::ActorId;
 
-// use plum_piece::PieceInfo;
-
+/// Convert actorid to prove id
+pub fn to_prove_id(actor_id: ActorId) -> Result<[u8; 32], AddressError> {
+    let addr = Address::new_id_addr(actor_id)?;
+    let mut res: [u8; 32] = Default::default();
+    let payload = addr.payload();
+    let len = std::cmp::min(payload.len(), res.len());
+    res[..len].copy_from_slice(&payload[..len]);
+    Ok(res)
+}
