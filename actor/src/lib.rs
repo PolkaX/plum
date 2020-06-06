53d94afc1bf9678055de784f38f67457963d2b9d
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-06-07 00:49:13 +0800

    crypto randomness is already done

--- actor/src/lib.rs
+++ actor/src/lib.rs
@@ -1,7 +1,6 @@
 // Copyright 2019-2020 PolkaX Authors. Licensed under GPL-3.0.
 
 mod builtin;
-pub mod crypto;
 
 pub use self::builtin::{
     account, cron, init, market, miner, multisig, network::*, paych, power, reward, system,
