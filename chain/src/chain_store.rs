755b6d9f00b0b75a746e18abbcddc2f5a975baaf
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-06-07 18:44:27 +0800

    Borrow draw_randomness()

---
+++ chain/src/chain_store.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+use blake2b_simd::Params;
+use byteorder::{BigEndian, WriteBytesExt};
+use plum_crypto::DomainSeparationTag;
+use plum_hashing::blake2b_256;
+use plum_types::ChainEpoch;
+use std::io::Write;
+
+/// Computes a pseudorandom 32 byte Vec
+pub fn draw_randomness(
+    rbase: &[u8],
+    pers: DomainSeparationTag,
+    round: ChainEpoch,
+    entropy: &[u8],
+) -> Result<[u8; 32]> {
+    let mut state = Params::new().hash_length(32).to_state();
+    state.write_i64::<BigEndian>(pers as i64)?;
+    let vrf_digest = blake2b_256(rbase);
+    state.write_all(&vrf_digest)?;
+    state.write_i64::<BigEndian>(round as i64)?;
+    state.write_all(entropy)?;
+    let mut ret = [0u8; 32];
+    ret.clone_from_slice(state.finalize().as_bytes());
+    Ok(ret)
+}
