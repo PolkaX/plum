63afd2caa06f0b932236dcefeee22210fae063db
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-06-09 22:08:02 +0800

    .

--- chain/src/chain_store.rs
+++ chain/src/chain_store.rs
@@ -1,7 +1,6 @@
 // Copyright 2019-2020 PolkaX Authors. Licensed under GPL-3.0.
 
 use anyhow::Result;
-use blake2b_simd::Params;
 use byteorder::{BigEndian, WriteBytesExt};
 use plum_crypto::DomainSeparationTag;
 use plum_hashing::blake2b_256;
@@ -15,13 +14,11 @@ pub fn draw_randomness(
     round: ChainEpoch,
     entropy: &[u8],
 ) -> Result<[u8; 32]> {
-    let mut state = Params::new().hash_length(32).to_state();
-    state.write_i64::<BigEndian>(pers as i64)?;
+    let mut data = Vec::new();
+    data.write_i64::<BigEndian>(pers as i64)?;
     let vrf_digest = blake2b_256(rbase);
-    state.write_all(&vrf_digest)?;
-    state.write_i64::<BigEndian>(round as i64)?;
-    state.write_all(entropy)?;
-    let mut ret = [0u8; 32];
-    ret.clone_from_slice(state.finalize().as_bytes());
-    Ok(ret)
+    data.write_all(&vrf_digest)?;
+    data.write_i64::<BigEndian>(round as i64)?;
+    data.write_all(entropy)?;
+    Ok(blake2b_256(data))
 }
