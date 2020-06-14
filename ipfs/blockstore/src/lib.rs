18d5ea674f100f78a4ea210530fd1e475bddbde9
koushiro <koushiro.cqx@gmail.com>
2020-06-14 20:23:38 +0800

    Fix BlockStore trait
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- ipfs/blockstore/src/lib.rs
+++ ipfs/blockstore/src/lib.rs
@@ -24,29 +24,31 @@ impl From<Box<dyn std::error::Error + Send + Sync>> for BlockStoreError {
     }
 }
 
+pub(crate) type Result<T> = std::result::Result<T, BlockStoreError>;
+
 /// BlockStore wraps a DataStore block-centered methods and provides a layer
 /// of abstraction which allows to add different caching strategies.
 pub trait BlockStore {
     /// Delete the block for given `cid`.
     /// If the `cid` is not in the block store, this method returns no error.
-    fn delete_block(&self, cid: &Cid) -> Result<(), BlockStoreError>;
+    fn delete_block(&mut self, cid: &Cid) -> Result<()>;
 
     /// Return whether the `cid` is mapped to a `block`.
-    fn has(&self, cid: &Cid) -> Result<bool, BlockStoreError>;
+    fn has(&self, cid: &Cid) -> Result<bool>;
 
     /// Retrieve the `block` named by `cid`.
-    fn get<B: Block>(&self, cid: &Cid) -> Result<B, BlockStoreError>;
+    fn get<B: Block>(&self, cid: &Cid) -> Result<B>;
 
     /// Return the CIDs mapped BlockSize.
-    fn get_size(&self, cid: &Cid) -> Result<usize, BlockStoreError>;
+    fn get_size(&self, cid: &Cid) -> Result<usize>;
 
     /// Put a given block to the underlying datastore
-    fn put<B: Block>(&mut self, block: B) -> Result<(), BlockStoreError>;
+    fn put<B: Block>(&mut self, block: B) -> Result<()>;
 
     /// Puts a slice of blocks at the same time using batching capabilities of
     /// the underlying datastore whenever possible.
-    fn put_many<B: Block>(&mut self, blocks: &[B]) -> Result<(), BlockStoreError>;
+    fn put_many<B: Block>(&mut self, blocks: &[B]) -> Result<()>;
 
     /// Specifies if every read block should be rehashed to make sure it matches its CID.
-    fn hash_on_read(&mut self, enable: bool);
+    fn hash_on_read(&mut self, enabled: bool);
 }
