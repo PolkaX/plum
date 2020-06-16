fdaaccea9942648c8fd65669aa437f8fbd5404e9
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-06-16 10:29:00 +0800

    Refactor fc::comm module a bit (#95)
    * Refactor fc::comm module a bit
    
    * Add some docs
    
    * More docs
    
    * .

--- primitives/fc/src/lib.rs
+++ primitives/fc/src/lib.rs
@@ -4,13 +4,9 @@
 
 #![deny(missing_docs)]
 
-mod comm;
+mod commcid;
 
-pub use self::comm::{
-    cid_to_data_commitment_v1, cid_to_piece_commitment_v1, cid_to_replica_commitment_v1,
-    commitment_to_cid, data_commitment_v1_to_cid, piece_commitment_v1_to_cid,
-    replica_commitment_v1_to_cid, CommCidErr, FILECOIN_CODEC_TYPE,
-};
+pub use self::commcid::*;
 
 use plum_address::{Address, AddressError};
 use plum_types::ActorId;
