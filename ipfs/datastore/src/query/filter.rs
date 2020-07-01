8a111f9942d8dd6ec3da7f1ad9f63308c202f5de
koushiro <koushiro.cqx@gmail.com>
2020-07-01 11:19:09 +0800

    Add filter and order of datastore query
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- ipfs/datastore/src/query/filter.rs
+++ ipfs/datastore/src/query/filter.rs
@@ -1,9 +1,137 @@
 // Copyright 2019-2020 PolkaX Authors. Licensed under GPL-3.0.
 
+use std::cmp::Ordering;
+use std::fmt;
+
+use crate::key::Key;
 use crate::query::Entry;
 
-///
-pub trait Filter {
-    ///
-    fn filter(entry: &Entry) -> bool;
+/// Filter is an object that tests ResultEntries.
+pub trait Filter: dyn_clone::DynClone + fmt::Debug + fmt::Display {
+    /// Return whether the entry passes the filter.
+    fn filter(&self, entry: &Entry) -> bool;
+}
+
+dyn_clone::clone_trait_object!(Filter);
+
+/// Filter comparison operator.
+#[doc(hidden)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug)]
+pub enum FilterOp {
+    Equal,
+    NotEqual,
+    GreaterThan,
+    GreaterThanEqual,
+    LessThan,
+    LessThanEqual,
+}
+
+impl fmt::Display for FilterOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            FilterOp::Equal => f.write_str("=="),
+            FilterOp::NotEqual => f.write_str("!="),
+            FilterOp::GreaterThan => f.write_str(">"),
+            FilterOp::GreaterThanEqual => f.write_str(">="),
+            FilterOp::LessThan => f.write_str("<"),
+            FilterOp::LessThanEqual => f.write_str("<="),
+        }
+    }
+}
+
+/// A object used to filter data by the key.
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Debug)]
+pub struct FilterKeyCompare {
+    key: Key,
+    op: FilterOp,
+}
+
+impl FilterKeyCompare {
+    /// Create a new FilterKeyCompare instance.
+    pub fn new(key: Key, op: FilterOp) -> Self {
+        Self { key, op }
+    }
+}
+
+impl fmt::Display for FilterKeyCompare {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "KEY {} {}", self.op, self.key)
+    }
+}
+
+impl Filter for FilterKeyCompare {
+    fn filter(&self, entry: &Entry) -> bool {
+        match self.op {
+            FilterOp::Equal => entry.key == self.key,
+            FilterOp::NotEqual => entry.key != self.key,
+            FilterOp::GreaterThan => entry.key > self.key,
+            FilterOp::GreaterThanEqual => entry.key >= self.key,
+            FilterOp::LessThan => entry.key < self.key,
+            FilterOp::LessThanEqual => entry.key <= self.key,
+        }
+    }
+}
+
+/// A object used to filter data by the value.
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Debug)]
+pub struct FilterValueCompare {
+    value: Vec<u8>,
+    op: FilterOp,
+}
+
+impl FilterValueCompare {
+    /// Create a new FilterValueCompare instance.
+    pub fn new<V: Into<Vec<u8>>>(value: V, op: FilterOp) -> Self {
+        Self {
+            value: value.into(),
+            op,
+        }
+    }
+}
+
+impl fmt::Display for FilterValueCompare {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "VALUE {} {:?}", self.op, self.value)
+    }
+}
+
+impl Filter for FilterValueCompare {
+    fn filter(&self, entry: &Entry) -> bool {
+        let order = entry.value.cmp(&self.value);
+        match self.op {
+            FilterOp::Equal => order == Ordering::Equal,
+            FilterOp::NotEqual => order != Ordering::Equal,
+            FilterOp::GreaterThan => order == Ordering::Greater,
+            FilterOp::GreaterThanEqual => order == Ordering::Greater || order == Ordering::Equal,
+            FilterOp::LessThan => order == Ordering::Less,
+            FilterOp::LessThanEqual => order == Ordering::Less || order == Ordering::Equal,
+        }
+    }
+}
+
+/// A object used to filter data by the prefix of key.
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Debug)]
+pub struct FilterKeyPrefix {
+    prefix: String,
+}
+
+impl FilterKeyPrefix {
+    /// Create a new FilterKeyPrefix instance.
+    pub fn new<S: Into<String>>(prefix: S) -> Self {
+        Self {
+            prefix: prefix.into(),
+        }
+    }
+}
+
+impl fmt::Display for FilterKeyPrefix {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "PREFIX({})", self.prefix)
+    }
+}
+
+impl Filter for FilterKeyPrefix {
+    fn filter(&self, entry: &Entry) -> bool {
+        entry.key.as_str().starts_with(&self.prefix)
+    }
 }
--- ipfs/datastore/src/query/order.rs
+++ ipfs/datastore/src/query/order.rs
@@ -1,11 +1,147 @@
 // Copyright 2019-2020 PolkaX Authors. Licensed under GPL-3.0.
 
 use std::cmp::Ordering;
+use std::fmt;
 
 use crate::query::Entry;
 
-///
-pub trait Order {
-    ///
-    fn compare(a: &Entry, b: &Entry) -> Ordering;
+/// Order is an object used to order objects
+pub trait Order: dyn_clone::DynClone + fmt::Debug + fmt::Display {
+    /// Returns an `Ordering` between `lhs` and `rhs`.
+    fn compare(&self, lhs: &Entry, rhs: &Entry) -> Ordering;
+}
+
+dyn_clone::clone_trait_object!(Order);
+
+/// A object used to order data by the key ascending.
+#[derive(Copy, Clone)]
+pub struct OrderByKey;
+
+impl fmt::Debug for OrderByKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "OrderByKey: {:p}", &entry_key_ascending)
+    }
+}
+
+impl fmt::Display for OrderByKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("KEY")
+    }
+}
+
+impl Order for OrderByKey {
+    fn compare(&self, lhs: &Entry, rhs: &Entry) -> Ordering {
+        OrderByFunction::new(entry_key_ascending).compare(lhs, rhs)
+    }
+}
+
+fn entry_key_ascending(lhs: &Entry, rhs: &Entry) -> Ordering {
+    lhs.key.cmp(&rhs.key)
+}
+
+/// A object used to order data by the key descending.
+#[derive(Copy, Clone)]
+pub struct OrderByKeyDescending;
+
+impl fmt::Debug for OrderByKeyDescending {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "OrderByKeyDescending: {:p}", &entry_key_descending)
+    }
+}
+
+impl fmt::Display for OrderByKeyDescending {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("desc(KEY)")
+    }
+}
+
+impl Order for OrderByKeyDescending {
+    fn compare(&self, lhs: &Entry, rhs: &Entry) -> Ordering {
+        OrderByFunction::new(entry_key_descending).compare(lhs, rhs)
+    }
+}
+
+fn entry_key_descending(lhs: &Entry, rhs: &Entry) -> Ordering {
+    rhs.key.cmp(&lhs.key)
+}
+
+/// A object used to order data by the value ascending.
+#[derive(Copy, Clone)]
+pub struct OrderByValue;
+
+impl fmt::Debug for OrderByValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "OrderByValue: {:p}", &entry_value_ascending)
+    }
+}
+
+impl fmt::Display for OrderByValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("VALUE")
+    }
+}
+
+impl Order for OrderByValue {
+    fn compare(&self, lhs: &Entry, rhs: &Entry) -> Ordering {
+        OrderByFunction::new(entry_value_ascending).compare(lhs, rhs)
+    }
+}
+
+fn entry_value_ascending(lhs: &Entry, rhs: &Entry) -> Ordering {
+    lhs.value.cmp(&rhs.value)
+}
+
+/// A object used to order data by the value descending.
+#[derive(Copy, Clone)]
+pub struct OrderByValueDescending;
+
+impl fmt::Debug for OrderByValueDescending {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "OrderByValueDescending: {:p}", &entry_value_descending)
+    }
+}
+
+impl fmt::Display for OrderByValueDescending {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("desc(VALUE)")
+    }
+}
+
+impl Order for OrderByValueDescending {
+    fn compare(&self, lhs: &Entry, rhs: &Entry) -> Ordering {
+        OrderByFunction::new(entry_value_descending).compare(lhs, rhs)
+    }
+}
+
+fn entry_value_descending(lhs: &Entry, rhs: &Entry) -> Ordering {
+    rhs.value.cmp(&lhs.value)
+}
+
+/// A object used to order data by the function.
+#[derive(Clone)]
+pub struct OrderByFunction(fn(&Entry, &Entry) -> Ordering);
+
+impl fmt::Debug for OrderByFunction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "OrderByFunction: {:p}", &self.0)
+    }
+}
+
+impl OrderByFunction {
+    /// Create a new OrderByFunction instance.
+    pub fn new(func: fn(&Entry, &Entry) -> Ordering) -> Self {
+        Self(func)
+    }
+}
+
+impl fmt::Display for OrderByFunction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("FN")
+    }
+}
+
+impl Order for OrderByFunction {
+    fn compare(&self, lhs: &Entry, rhs: &Entry) -> Ordering {
+        (self.0)(lhs, rhs)
+    }
 }
