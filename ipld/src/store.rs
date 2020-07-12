f2914c56146e7122f56eda4d9ca05327e22b7521
Qinxuan Chen <koushiro.cqx@gmail.com>
2020-07-12 14:08:15 +0800

    Add IPLD store (#109)
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

---
+++ ipld/src/store.rs
@@ -0,0 +1,35 @@
+// Copyright 2019-2020 PolkaX Authors. Licensed under GPL-3.0.
+
+use anyhow::Result;
+use cid::Cid;
+
+use ipfs_block::IpfsBlock;
+use ipfs_blockstore::BlockStore;
+
+/// IpldStore wraps block store and provides an interface for storing and retrieving CBOR encoded data.
+pub trait IpldStore: BlockStore {
+    /// Get an object from the block store by the cid.
+    fn get<T>(&self, cid: &Cid) -> Result<Option<T>>
+    where
+        T: for<'b> minicbor::Decode<'b>,
+    {
+        match BlockStore::get(self, cid)? {
+            Some(block) => {
+                let data = (*block).data();
+                Ok(Some(minicbor::decode(data)?))
+            }
+            None => Ok(None),
+        }
+    }
+
+    /// Put an object into the block store.
+    fn put<T>(&mut self, value: T) -> Result<Cid>
+    where
+        T: minicbor::Encode,
+    {
+        let block = IpfsBlock::new(value);
+        let cid = block.cid().clone();
+        BlockStore::put(self, block)?;
+        Ok(cid)
+    }
+}
